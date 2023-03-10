7d3a290ba19bb295baf19cf094f5203ca66dabac
Tatiana Sverchkova <swerchkova.tatiana@gmail.com>
2023-03-11 01:17:54 +0400

    Added media

--- src/test/java/specs/Specs.java
+++ src/test/java/specs/Specs.java
@@ -7,7 +7,8 @@ import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
 
 import static io.restassured.RestAssured.with;
-import static io.restassured.filter.log.LogDetail.*;
+import static io.restassured.filter.log.LogDetail.BODY;
+import static io.restassured.filter.log.LogDetail.STATUS;
 
 public class Specs {
     public static RequestSpecification request = with()
